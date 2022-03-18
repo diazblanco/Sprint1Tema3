34ad1e1788095c3db6963393edb28ac71172d8c9
diazblanco <diazblancodesign@gmail.com>
2022-03-18 09:40:43 +0100

    PHP bàsics - Nivell 2 - Exercici 1

---
+++ Nivell2/ex1.php
@@ -0,0 +1,12 @@
+<?php
+$valor1=2;
+$valor2=2;
+
+if ($valor1 != $valor2) {
+    echo "La suma de $valor1 + $valor2 és " . $valor1 + $valor2;
+} else {
+    echo "El doble de la suma de $valor1 + $valor2 és " . ($valor1 + $valor2) * 2;
+}
+
+
+?>
